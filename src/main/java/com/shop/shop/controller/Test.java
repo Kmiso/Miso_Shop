9eeeee0f68b0819cd75a2d520b8a4d0bb0132bf6
Miso <kmiso9720@gmail.com>
2022-12-17 00:05:58 +0900

    ADMIN Item Sell
    관리자 상품 판매 화면 구현중

---
+++ src/main/java/com/shop/shop/controller/Test.java
@@ -0,0 +1,14 @@
+package com.shop.shop.controller;
+
+import java.util.*;
+
+public class Test {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println(answer[1]);
+
+        return answer;
+    }
+}
