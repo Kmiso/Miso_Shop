408423bb58bdd98dbb0cb8df281a634ef4eb36d7
Miso <kmiso9720@gmail.com>
2023-01-04 23:24:33 +0900

    OrderServiceTest
    saveItem() 작성

--- src/test/java/com/shop/shop/service/OrderServiceTest.java
+++ src/test/java/com/shop/shop/service/OrderServiceTest.java
@@ -1,5 +1,7 @@
 package com.shop.shop.service;
 
+import com.shop.shop.constant.ItemSellStatus;
+import com.shop.shop.entity.Item;
 import com.shop.shop.repository.ItemRepository;
 import com.shop.shop.repository.MemberRepository;
 import com.shop.shop.repository.OrderRepository;
@@ -23,4 +25,14 @@ class OrderServiceTest {
     ItemRepository itemRepository;
     @Autowired
     MemberRepository memberRepository;
+
+    public Item saveItem() {
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        return itemRepository.save(item);
+    }
 }
