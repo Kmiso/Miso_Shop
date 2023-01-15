6bffb1bc2b6deda4fc738816373646af2b460fbf
Miso <kmiso9720@gmail.com>
2023-01-15 22:31:47 +0900

    상품 검색 기능
    ItemSearchDto

--- src/main/java/com/shop/shop/repository/ItemRepositoryCustom.java
+++ src/main/java/com/shop/shop/repository/ItemRepositoryCustom.java
@@ -1,6 +1,7 @@
 package com.shop.shop.repository;
 
 import com.shop.shop.dto.ItemSearchDto;
+import com.shop.shop.dto.MainItemDto;
 import com.shop.shop.entity.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,4 +9,5 @@ import org.springframework.data.domain.Pageable;
 public interface ItemRepositoryCustom {
     // 상품 조회 조건을 담고 있는 itemSearchDto 객체와 페이징 정보를 담고 있는 pageable 객체를 파라미터로 받는 getAdminItemPage 정의
     Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
 }
